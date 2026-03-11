7e54a5f20088a49d133da0436487ed4ef122b270
taniachavezv <taniachavezv11@gmail.com>
2026-03-11 13:29:12 -0600

    avanse de curso

---
+++ EquivaFood/comentarios.dart
@@ -0,0 +1,21 @@
+void main(){
+
+  //declaración e inicialización de variables
+  String NOMBRE = 'Hola a todos';
+  int edad_1 = 20;
+  double _precio = 550.20;
+  bool aCTivo = false; //situación del cliente
+  var Codigo = false;
+
+  //uso de constantes
+  final PI = 3.1416;
+  const ValorFijo = 600;
+
+  //generación de info por consola
+
+  print(NOMBRE);
+  print(PI);
+  print(edad_1);
+  print(_precio);
+
+}
